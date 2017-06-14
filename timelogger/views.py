8271bd4fe42e639eff96a4a513970efc9ec62c59
Johhan Barba <johhanjohndennismartin@gmail.com>
2017-06-14 17:21:36 +0800

    added timelogger initial files

---
+++ timelogger/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'timelogger/login.html')
+
+
+@login_required
+def home(request):
+    return render(request, 'timelogger/home.html')
